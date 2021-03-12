ccc1eb7fec8cdab6c625a10052146a81ab7326d2
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2021-03-12 13:19:59 -0300

    feat: add props in react

--- fundamentals/src/component/RepositoryList.jsx
+++ fundamentals/src/component/RepositoryList.jsx
@@ -1,4 +1,10 @@
-const repositoryName = 'unform2';
+import { RepositoryItem } from "./RepositoryItem";
+
+const repositoryData = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'https://github.com/unform/unform'
+};
 
 export function RepositoryList() {
   return (
@@ -6,41 +12,11 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-        
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repositoryData} />
+        <RepositoryItem repository={repositoryData} />
+        <RepositoryItem repository={repositoryData} />
+        <RepositoryItem repository={repositoryData} />
+        <RepositoryItem repository={repositoryData} />
       </ul>
     </section>
   )
